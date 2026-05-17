5154766d9331b948aa219e9f213be3643ac76e91
Somilgupta07 <guptasomil0567@gmail.com>
2026-05-17 17:12:32 +0530

    completed loss function Training loop AdaIN layer

--- train.py
+++ train.py
@@ -5,6 +5,7 @@ import torch.optim as optim
 from utils.utils import *
 from torch.utils.data import DataLoader
 from utils.models import *
+from tqdm import tqdm
 
 
 def parse_arguments():
@@ -39,7 +40,7 @@ def parse_arguments():
     parser.add_argument(
         '--final_size',
         type=int,
-        default=512,
+        default=256,
         help='Size of final image'
     )
     parser.add_argument(
@@ -71,6 +72,22 @@ def parse_arguments():
                         help='Learning rate')
     parser.add_argument('--lr_decay', type=float, default=5e-5,
                         help='Learning rate decay')
+    parser.add_argument('--epochs', type=int, default=1,
+                        help='Number of epochs')
+    
+    parser.add_argument('--content_weight', type=float, default=1.0,
+                        help='Content weight')
+    parser.add_argument('--style_weight', type=float, default=5,
+                        help='Style weight')
+    
+    parser.add_argument('--log_interval', type=int, default=1,
+                        help='Log interval')
+    
+    parser.add_argument('--save_interval', type=int, default=2,
+                        help='Save interval')
+    
+    parser.add_argument('--resume', action='store_true', default=False,
+                        help='Resume training')
 
 
 
@@ -124,6 +141,64 @@ def main():
     )
 
     print('Training...')
+
+    mse_loss=torch.nn.MSELoss()
+    encoder.eval()
+    running_loss=None
+    running_closs=None
+    running_sloss=None
+
+    for epoch in range(args.epochs):
+        progress_bar=tqdm(zip(content_dataloader,style_dataloader),
+                          total=min(len(content_dataloader),len(style_dataloader)))
+        
+        running_closs=0
+        running_loss=0
+        running_sloss=0
+
+        for content_batch,style_batch in progress_bar:
+            content_batch=content_batch.to(device)
+            style_batch=style_batch.to(device)
+
+            c_feats=encoder(content_batch)
+            s_feats=encoder(style_batch)
+
+            t=adapative_instance_normalization(c_feats[-1],s_feats[-1])
+
+            g=decoder(t)
+
+            g_feats=encoder(g)
+
+            loss_c=mse_loss(g_feats[-1],t)*args.content_weight
+
+
+            loss_s=0
+            for g_f,s_f in zip(g_feats,s_feats):
+                g_mean,g_std=calc_mean_std(g_f)
+                s_mean,s_std=calc_mean_std(s_f)
+                loss_s+=mse_loss(g_mean,s_mean)+mse_loss(g_std,s_std)
+
+            loss_s=loss_s*args.style_weight
+            loss=loss_c+loss_s
+
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+            progress_bar.set_description(f'Loss:{loss.item():4f}, Content Loss: {loss_c.item():4f}, Style Loss: {loss_s.item():4f}')
+
+            running_loss += loss.item()
+            running_closs += loss_c.item()
+            running_sloss += loss_s.item()
+
+        scheduler.step()
+        running_loss /= len(content_dataloader)
+        running_closs /= len(content_dataloader)
+        running_sloss /= len(content_dataloader)
+
+        if(epoch+1)%args.log_interval==0:
+            tqdm.write(f'Iter {epoch+1}: Loss:{running_loss:4f}, Content Loss: {running_closs:4f}, Style Loss: {running_sloss:4f}')
+
 
 
 if __name__ == '__main__':
